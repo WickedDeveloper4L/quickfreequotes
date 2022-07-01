925c6215c15769f3e623d09aa23d36ad329f65a6
Ahia Kleine Chris <101099456+WickedDeveloper4L@users.noreply.github.com>
2022-07-02 00:29:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+
+const url = "https://api.quotable.io/random";
+
+let getQuote = () => {
+    fetch(url)
+      .then((data) => data.json())
+      .then((item) => {
+        quote.innerText = item.content;
+        author.innerText = item.author;
+      });
+};
+btn.addEventListener("click", getQuote);
