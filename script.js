8aeccf2b9ce782b5b6852f6d60c66c697ce0056f
Ahia Kleine Chris <101099456+WickedDeveloper4L@users.noreply.github.com>
2022-07-04 13:28:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 let intro = alert("Quick free Quotes built by Chris, new features in this version; No Repeat of quotes, Sleek UI and vast library.");
-
+let copy = document.getElementById("copy");
+let board = document.getElementById("board");
 
 const url = "https://api.quotable.io/random";
 
@@ -13,7 +14,22 @@ let getQuote = () => {
         quote.innerText = item.content;
         author.innerText = item.author;
       });
+
+      copy.innerHTML = '<i class="fa-regular fa-clipboard"></i>Copy Quote';
 };
+
+let copyQuote = () => {
+  const copyText = document.getElementById("board").textContent;
+  const textArea = document.createElement('textarea');
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  copy.innerHTML = 'Quote copied';
+  textArea.remove();
+};
+
 
 window.addEventListener("load", intro);
 btn.addEventListener("click", getQuote);
+copy.addEventListener("click", copyQuote);
